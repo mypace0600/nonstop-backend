063035bcd60954fbcf98d1c8595c11dd97b607ee
hyeonsu <mypace0600@gmail.com>
2025-12-10 12:51:24 +0900

    Initial commit

---
+++ src/main/java/com/app/nonstop/NonstopApplication.java
@@ -0,0 +1,13 @@
+package com.app.nonstop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonstopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonstopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/nonstop/NonstopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.nonstop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonstopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
