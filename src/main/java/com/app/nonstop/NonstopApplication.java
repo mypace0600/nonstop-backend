06f1c4dd83a717cb82f9e8c568bb5debeebcfcc1
hyeonsu <mypace0600@gmail.com>
2025-12-26 18:52:58 +0900

    시스템 리펙토링

--- src/main/java/com/app/nonstop/NonstopApplication.java
+++ src/main/java/com/app/nonstop/NonstopApplication.java
@@ -1,13 +1,11 @@
 package com.app.nonstop;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@MapperScan("com.app.nonstop.mapper")
 public class NonstopApplication {
 
 	public static void main(String[] args) {
